9be248d94fc1f17d1cea7de36c22379a23845b86
Alan Platt <alan.platt@capablue.com>
2016-08-04 16:10:17 +0100

    INFRA-2 Add testing and supporting script
    * Add some test for aws.py basically covering some of the main definitions
    within the code.
    * Add test.sh to allow easy setup and running for tests and pep8 syntax
    checks.
    
    * Also added requirements-test.txt to help with installation of
    dependancies for tests.

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Exit on any error
+set -e
+
+echo "Creating virtualenv..."
+VENV_NAME=".venv"
+virtualenv ${VENV_NAME}
+source "${VENV_NAME}/bin/activate"
+
+echo "Installing requirements..."
+pip install -q -r requirements.txt -r requirements-tests.txt
+
+echo "Cleaning .pyc files..."
+find . -iname "*.pyc" -delete
+
+echo "Running tests..."
+nosetests --with-coverage --cover-erase --cover-package=aws_assume_role
+
+echo "Running flake8..."
+flake8 aws_assume_role/ tests/
+echo "OK"
+
+echo "Deactivating virtualenv..."
+deactivate
