d7c8d83b227182d4c68afeb0b0813a69e9f47bac
Alan Platt <alan.platt@capablue.com>
2016-08-11 11:49:27 +0100

    INFRA-2 Add support for Travis CI builds
    * add appropriate configuration file for Travis CI
    * update test.sh for use with travis

--- test.sh
+++ test.sh
@@ -3,15 +3,21 @@
 # Exit on any error
 set -e
 
-echo "Creating virtualenv..."
-VENV_NAME=".venv"
-if ! [ -e "${VENV_NAME}/bin/activate" ]; then
-  virtualenv ${VENV_NAME}
-fi
-source "${VENV_NAME}/bin/activate"
 
-echo "Installing requirements..."
-pip install -q -r requirements.txt -r requirements-tests.txt
+if [[ $TRAVIS != 'true' ]]; then
+
+  VENV_NAME=".venv"
+  if ! [ -e "${VENV_NAME}/bin/activate" ]; then
+    echo "Creating virtualenv..."
+    virtualenv ${VENV_NAME}
+  fi
+
+  source "${VENV_NAME}/bin/activate"
+
+  echo "Installing requirements..."
+  pip install -q -r requirements.txt -r requirements-tests.txt
+
+fi
 
 echo "Cleaning .pyc files..."
 find . -iname "*.pyc" -delete
@@ -21,7 +27,5 @@ nosetests --with-coverage --cover-erase --cover-package=aws_assume_role
 
 echo "Running flake8..."
 flake8 aws_assume_role/ tests/
-echo "OK"
 
-echo "Deactivating virtualenv..."
-deactivate
+echo "Done!!"
