7c1e25d76ee4b80832374fb90dfe73c20af05e56
Alan Platt <alan.platt@capablue.com>
2016-10-05 15:32:48 +0100

    FIX Add 0 to start of test integer
    This is to catch ints that start with 0

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -9,8 +9,8 @@ class TestAWS(unittest.TestCase):
         """
         If passed an integer function should return said integer
         """
-        with patch('__builtin__.raw_input', return_value='1234') as _raw_input:
-            self.assertEquals(get_MFA_token(), '1234')
+        with patch('__builtin__.raw_input', return_value='0123') as _raw_input:
+            self.assertEquals(get_MFA_token(), '0123')
             _raw_input.assert_called_once_with("Enter the MFA code: ")
 
     def test_get_MFA_token_bad_token(self):
